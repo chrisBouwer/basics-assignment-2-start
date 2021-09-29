f0cf275241a4e983ccb48f613a0bd9daa67e11b0
chrisBouwer <chrisbouwer01@gmail.com>
2021-09-29 09:45:25 +0200

    alert function on event click

--- assignment.js
+++ assignment.js
@@ -1,1 +1,20 @@
 const task3Element = document.getElementById('task-3');
+
+function alertMe() {
+  alert('You have been alerted!');
+}
+
+function alertName(name) {
+  alert('Alert! ' + name);
+}
+
+alertMe();
+alertName('Christiaan');
+
+task3Element.addEventListener('click', alertMe);
+
+function concatStrings(str1, str2, str3) {
+  return str1 + str2 + str3;
+}
+
+alertName(concatStrings('I ', '♥ ', 'U'));
